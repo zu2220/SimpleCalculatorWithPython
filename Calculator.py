d0676dae732c241a13b2f3c877ca6cad081dc434
zu2220 <u22206853@utp.edu.pe>
2025-08-10 19:23:31 -0500

    new changes

--- Calculator.py
+++ Calculator.py
@@ -1,10 +1,28 @@
 def addition(a, b):
     return a + b
 
+def substraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
 operation = input("Enter the type of operation you wanna do: ")
 
 match (operation):
     case "addition":
         a = input("give me the firts number: ")
         b = input("give me the second number: ")
-        print(f"the result is {addition(a,b)}")    
+        print(f"the result is {addition(a,b)}")
+
+    case "substraction":
+        a = input("give me the minuend: ")
+        b = input("give me the subtrahend: ")
+        print(f"the difference is {substraction(a, b)}")
+
+    case _:
+        print("None of the operations were specified!")
+             
